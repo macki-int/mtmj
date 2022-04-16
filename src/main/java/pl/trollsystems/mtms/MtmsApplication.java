6cc36ea0df4e21d92ff82eeb66d0a85f55a201e5
macki-int <macki@int.pl>
2022-04-16 17:37:49 +0200

    add spring boot

---
+++ src/main/java/pl/trollsystems/mtms/MtmsApplication.java
@@ -0,0 +1,13 @@
+package pl.trollsystems.mtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/trollsystems/mtms/MtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.trollsystems.mtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
