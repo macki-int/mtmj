03d470151899ca52e99ff02295128500dc86f8ed
macki-int <macki@int.pl>
2022-04-16 19:56:09 +0200

    add findById in UserController class

--- src/main/java/pl/trollsystems/mtms/controller/UserController.java
+++ src/main/java/pl/trollsystems/mtms/controller/UserController.java
@@ -3,9 +3,13 @@ package pl.trollsystems.mtms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.trollsystems.mtms.model.User;
 import pl.trollsystems.mtms.repository.UserRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -20,5 +24,16 @@ public class UserController {
     @GetMapping
     public ResponseEntity<?> findAll(){
         return ResponseEntity.ok().body(userRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if(userOptional.isPresent()) {
+            return ResponseEntity.ok().body(userOptional);
+        }
+        
+        return ResponseEntity.noContent().build();
     }
 }
