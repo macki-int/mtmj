22893728c43ddbe3970059d9265fceb9bc8ece55
macki-int <macki@int.pl>
2022-04-17 19:35:05 +0200

    add setters and getters in TransmitterParameterReading class

--- src/main/java/pl/trollsystems/mtms/model/TransmitterParameterReading.java
+++ src/main/java/pl/trollsystems/mtms/model/TransmitterParameterReading.java
@@ -19,4 +19,68 @@ public class TransmitterParameterReading {
     private String batteryCapacity;
     private String humidity;
     private String batteryVoltage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataTimeReading() {
+        return dataTimeReading;
+    }
+
+    public void setDataTimeReading(LocalDateTime dataTimeReading) {
+        this.dataTimeReading = dataTimeReading;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getSignalLevel() {
+        return signalLevel;
+    }
+
+    public void setSignalLevel(String signalLevel) {
+        this.signalLevel = signalLevel;
+    }
+
+    public String getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(String batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getBatteryVoltage() {
+        return batteryVoltage;
+    }
+
+    public void setBatteryVoltage(String batteryVoltage) {
+        this.batteryVoltage = batteryVoltage;
+    }
 }
