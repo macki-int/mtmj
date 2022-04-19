98fd01966bad9923ae97b319068701cbba089067
marekj <macki@int.pl>
2022-04-19 23:27:18 +0200

    add SplitReadout class

---
+++ src/main/java/pl/trollsystems/mtms/model/SplitReadout.java
@@ -0,0 +1,147 @@
+package pl.trollsystems.mtms.model;
+
+import java.util.List;
+
+public class SplitReadout {
+    //#T
+    // /s
+    private String _Ts;
+    // /p
+    private String _Tp;
+    // /m
+    private String _Tm;
+    //#M
+    // /b
+    private List<String> _Mb;
+    // /c
+    private String _Mc;
+    //#I
+    // /n
+    private String _In;
+    // /s
+    private String _Is;
+    // /b
+    private String _Ib;
+    // /e
+    private String _Ie;
+    // /f
+    private String _If;
+    // /h
+    private String _Ih;
+    // /v
+    private String _Iv;
+    //#G
+    // /a
+    private String _Ga;
+    //#E
+    // /e
+//    private String _Ee;
+    //#X
+    // /a
+//    private String _Xa;
+
+
+    public String get_Ts() {
+        return _Ts;
+    }
+
+    public void set_Ts(String _Ts) {
+        this._Ts = _Ts;
+    }
+
+    public String get_Tp() {
+        return _Tp;
+    }
+
+    public void set_Tp(String _Tp) {
+        this._Tp = _Tp;
+    }
+
+    public String get_Tm() {
+        return _Tm;
+    }
+
+    public void set_Tm(String _Tm) {
+        this._Tm = _Tm;
+    }
+
+    public List<String> get_Mb() {
+        return _Mb;
+    }
+
+    public void set_Mb(List<String> _Mb) {
+        this._Mb = _Mb;
+    }
+
+    public String get_Mc() {
+        return _Mc;
+    }
+
+    public void set_Mc(String _Mc) {
+        this._Mc = _Mc;
+    }
+
+    public String get_In() {
+        return _In;
+    }
+
+    public void set_In(String _In) {
+        this._In = _In;
+    }
+
+    public String get_Is() {
+        return _Is;
+    }
+
+    public void set_Is(String _Is) {
+        this._Is = _Is;
+    }
+
+    public String get_Ib() {
+        return _Ib;
+    }
+
+    public void set_Ib(String _Ib) {
+        this._Ib = _Ib;
+    }
+
+    public String get_Ie() {
+        return _Ie;
+    }
+
+    public void set_Ie(String _Ie) {
+        this._Ie = _Ie;
+    }
+
+    public String get_If() {
+        return _If;
+    }
+
+    public void set_If(String _If) {
+        this._If = _If;
+    }
+
+    public String get_Ih() {
+        return _Ih;
+    }
+
+    public void set_Ih(String _Ih) {
+        this._Ih = _Ih;
+    }
+
+    public String get_Iv() {
+        return _Iv;
+    }
+
+    public void set_Iv(String _Iv) {
+        this._Iv = _Iv;
+    }
+
+    public String get_Ga() {
+        return _Ga;
+    }
+
+    public void set_Ga(String _Ga) {
+        this._Ga = _Ga;
+    }
+}
