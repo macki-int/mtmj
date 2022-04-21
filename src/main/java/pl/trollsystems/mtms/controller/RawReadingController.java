21f641d431addba90f9e20a88152307e74d91619
macki-int <macki@int.pl>
2022-04-21 23:03:51 +0200

    add RawReadingController class

---
+++ src/main/java/pl/trollsystems/mtms/controller/RawReadingController.java
@@ -0,0 +1,27 @@
+package pl.trollsystems.mtms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.trollsystems.mtms.model.RawReading;
+import pl.trollsystems.mtms.repository.RawReadingRepository;
+
+@RestController
+@RequestMapping("/uploads")
+public class RawReadingController {
+    private RawReadingRepository rawReadingRepository;
+
+    @Autowired
+    public RawReadingController(RawReadingRepository rawReadingRepository) {
+        this.rawReadingRepository = rawReadingRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> add(@RequestBody RawReading rawReading){
+        System.out.println(rawReading.toString());
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
