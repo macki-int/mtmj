65acd47048765460d5e6c5263a892bc20dc29ca4
macki-int <macki@int.pl>
2022-04-24 23:51:27 +0200

    rename interface RawReadingController on RawReadoutController

---
+++ src/main/java/pl/trollsystems/mtms/controller/RawReadoutController.java
@@ -0,0 +1,26 @@
+package pl.trollsystems.mtms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.trollsystems.mtms.model.RawReadout;
+import pl.trollsystems.mtms.repository.RawReadoutRepository;
+
+@RestController
+@RequestMapping("/uploads")
+public class RawReadingController {
+    private RawReadoutRepository rawReadoutRepository;
+
+    @Autowired
+    public RawReadingController(RawReadoutRepository rawReadoutRepository) {
+        this.rawReadoutRepository = rawReadoutRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> add(@RequestBody RawReadout rawReadout){
+        rawReadoutRepository.save(rawReadout);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
