3eacea6cc20abf533abeeb8786fbc76b47c271b1
macki-int <macki@int.pl>
2022-04-29 01:06:00 +0200

    add deserialize json file in add method

--- src/main/java/pl/trollsystems/mtms/controller/RawReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/RawReadoutController.java
@@ -1,11 +1,21 @@
 package pl.trollsystems.mtms.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.trollsystems.mtms.model.RawReadout;
 import pl.trollsystems.mtms.repository.RawReadoutRepository;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/uploads")
@@ -18,8 +28,25 @@ public class RawReadoutController {
     }
 
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody RawReadout rawReadout){
-        rawReadoutRepository.save(rawReadout);
+    public ResponseEntity<?> add(@RequestBody String string) {
+
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
+                (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) -> {
+
+            try{
+                return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            } catch (DateTimeParseException e){
+                return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+            }
+        }).create();
+        Type listType = new TypeToken<ArrayList<RawReadout>>() {}.getType();
+        ArrayList<RawReadout> rawReadoutList = gson.fromJson(string, listType);
+
+        for (RawReadout rawReadout : rawReadoutList) {
+            rawReadoutRepository.save(rawReadout);
+        }
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
