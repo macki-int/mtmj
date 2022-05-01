3a0fa47ab261189deb8b19a24fcba0dccc89d377
macki-int <macki@int.pl>
2022-05-01 13:02:54 +0200

    add method findAllWhereRawImportIsFalse in RawReadoutRepository interface

--- src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
@@ -1,9 +1,14 @@
 package pl.trollsystems.mtms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.RawReadout;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RawReadoutRepository extends JpaRepository<RawReadout, Long> {
+    Optional<RawReadout> findAllWhereRawImportIsFalse();
 }
