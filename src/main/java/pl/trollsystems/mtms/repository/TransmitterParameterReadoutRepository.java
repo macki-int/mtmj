f68f4b1047a4619d75c00c4ecfb7eff5c25cbdbf
macki-int <macki@int.pl>
2022-05-02 21:08:26 +0200

    add import model TransmitterParameterReadout in repository

--- src/main/java/pl/trollsystems/mtms/repository/TransmitterParameterReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/TransmitterParameterReadoutRepository.java
@@ -1,6 +1,7 @@
 package pl.trollsystems.mtms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.trollsystems.mtms.model.TransmitterParameterReadout;
 
 public interface TransmitterParameterReadoutRepository extends JpaRepository<TransmitterParameterReadout, Long> {
 }
