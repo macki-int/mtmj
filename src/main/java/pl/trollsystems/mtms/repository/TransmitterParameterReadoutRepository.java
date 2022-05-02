3f3fc97c7a6412ca8272e7dcef6c9e202a13cb77
macki-int <macki@int.pl>
2022-05-02 21:07:33 +0200

    add TransmitterParameterReadoutRepository interface

---
+++ src/main/java/pl/trollsystems/mtms/repository/TransmitterParameterReadoutRepository.java
@@ -0,0 +1,6 @@
+package pl.trollsystems.mtms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransmitterParameterReadoutRepository extends JpaRepository<TransmitterParameterReadout, Long> {
+}
