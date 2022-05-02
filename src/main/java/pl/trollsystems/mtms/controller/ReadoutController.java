504907055630de479f63b363119d14e465d02419
macki-int <macki@int.pl>
2022-05-02 16:07:59 +0200

    add findAll method in ReadoutController class

--- src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
@@ -1,6 +1,8 @@
 package pl.trollsystems.mtms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.trollsystems.mtms.repository.ReadoutRepository;
@@ -13,5 +15,9 @@ public class ReadoutController {
     @Autowired
     public ReadoutController(ReadoutRepository readoutRepository) {
         this.readoutRepository = readoutRepository;
+    }
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        return ResponseEntity.ok().body(readoutRepository.findAll());
     }
 }
