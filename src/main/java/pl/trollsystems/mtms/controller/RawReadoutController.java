14ca04cbe663926266f10ea4c114a0db329cb422
macki-int <macki@int.pl>
2022-05-02 21:04:16 +0200

    add method makeImport() in RawReadoutController class

--- src/main/java/pl/trollsystems/mtms/controller/RawReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/RawReadoutController.java
@@ -9,13 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.trollsystems.mtms.model.RawReadout;
+import pl.trollsystems.mtms.model.Readout;
 import pl.trollsystems.mtms.repository.RawReadoutRepository;
+import pl.trollsystems.mtms.service.ReadoutParser;
 
 import java.lang.reflect.Type;
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -35,15 +39,16 @@ public class RawReadoutController {
         Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class,
                 (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) -> {
 
-            try{
-                return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            } catch (DateTimeParseException e){
-                return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
-            }
-        }).create();
-        Type listType = new TypeToken<ArrayList<RawReadout>>() {}.getType();
+                    try {
+                        return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    } catch (DateTimeParseException e) {
+                        return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS"));
+                    }
+                }).create();
+        Type listType = new TypeToken<ArrayList<RawReadout>>() {
+        }.getType();
         ArrayList<RawReadout> rawReadoutList = gson.fromJson(string, listType);
 
         for (RawReadout rawReadout : rawReadoutList) {
@@ -54,8 +59,21 @@ public class RawReadoutController {
     }
 
     @GetMapping("/non-imports")
-    public ResponseEntity<?> findAllWithoutImport(){
+    public ResponseEntity<?> findAllWithoutImport() {
 
         return ResponseEntity.ok().body(rawReadoutRepository.findByRawImportFalse());
+    }
+
+    @GetMapping("/do-imports")
+    public ResponseEntity<?> makeImport() {
+        List<RawReadout> rawReadouts = rawReadoutRepository.findByRawImportFalse();
+        if(rawReadouts.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        ReadoutParser readoutParser = new ReadoutParser();
+        List<Readout> readouts = readoutParser.parseByFactor(rawReadouts);
+
+        return ResponseEntity.ok().build();
     }
 }
