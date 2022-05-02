82f4197b82e4387abd947e57f51a85cc6192053e
macki-int <macki@int.pl>
2022-05-02 16:31:55 +0200

    change declaration method on findByRawImportFalse in RawReadoutRepository interface

--- src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface RawReadoutRepository extends JpaRepository<RawReadout, Long> {
-    Optional<RawReadout> findAllWhereRawImportIsFalse();
+    List<RawReadout> findByRawImportFalse();
 }
