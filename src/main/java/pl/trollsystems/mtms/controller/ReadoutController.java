9c27f24f09419670b1f7a6c53a12ed589edb7b23
macki-int <macki@int.pl>
2022-05-02 16:01:38 +0200

    add ReadoutController class

---
+++ src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
@@ -0,0 +1,17 @@
+package pl.trollsystems.mtms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.trollsystems.mtms.repository.ReadoutRepository;
+
+@RestController
+@RequestMapping("/readouts")
+public class ReadoutController {
+    private ReadoutRepository readoutRepository;
+
+    @Autowired
+    public ReadoutController(ReadoutRepository readoutRepository) {
+        this.readoutRepository = readoutRepository;
+    }
+}
