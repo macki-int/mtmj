ca41bc6164ba52dd8c1e950f684de42d11a16341
macki-int <macki@int.pl>
2022-05-03 11:32:16 +0200

    add annotation @Repository in TransmitterParameterReadoutRepository

--- src/main/java/pl/trollsystems/mtms/repository/TransmitterParameterReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/TransmitterParameterReadoutRepository.java
@@ -1,7 +1,9 @@
 package pl.trollsystems.mtms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.TransmitterParameterReadout;
 
+@Repository
 public interface TransmitterParameterReadoutRepository extends JpaRepository<TransmitterParameterReadout, Long> {
 }
