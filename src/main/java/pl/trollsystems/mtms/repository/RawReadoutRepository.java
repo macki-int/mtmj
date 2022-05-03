29bdddbf0277721286bf2ca259e5201208674739
macki-int <macki@int.pl>
2022-05-03 11:34:03 +0200

    add declaration save method in RawReadoutRepository

--- src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/RawReadoutRepository.java
@@ -1,14 +1,17 @@
 package pl.trollsystems.mtms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.RawReadout;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RawReadoutRepository extends JpaRepository<RawReadout, Long> {
     List<RawReadout> findByRawImportFalse();
+//    @Modifying
+//    @Query("update RawReadout r set r.rawImport = ?2 where r.id = ?1")
+//    void updateByIdRawImport(Long id, Boolean mark);
+
+    RawReadout save(RawReadout rawReadout);
 }
