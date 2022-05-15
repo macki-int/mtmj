44d389e72350c9d3ab213d822cfac731b036f745
marekj <macki@int.pl>
2022-05-15 20:36:55 +0200

    add default method findAllByOrderByReadoutDataTimeAsc in ReadoutRepository interface

--- src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.Readout;
 
+import java.util.List;
+
 @Repository
 public interface ReadoutRepository extends JpaRepository<Readout, Long> {
+    List<Readout> findAllByOrderByReadoutDataTimeAsc();
 }
