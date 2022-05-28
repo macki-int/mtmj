b172d209ed61ae680fb28a18b99210acca963270
marekj <macki@int.pl>
2022-05-28 21:36:52 +0200

    add DTO ReadoutDescriptionOnly class

---
+++ src/main/java/pl/trollsystems/mtms/DTO/ReadoutDescriptionOnly.java
@@ -0,0 +1,13 @@
+package pl.trollsystems.mtms.DTO;
+
+public class ReadoutDescriptionOnly {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
