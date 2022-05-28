6752e571373525b07f3436c03442a55cf182da8d
marekj <macki@int.pl>
2022-05-28 22:46:05 +0200

    add patch methode updateDescriptionById in ReadoutController

--- src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
@@ -3,8 +3,11 @@ package pl.trollsystems.mtms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.trollsystems.mtms.DTO.ReadoutDescriptionOnlyDTO;
 import pl.trollsystems.mtms.model.Readout;
 import pl.trollsystems.mtms.repository.ReadoutRepository;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -26,4 +29,18 @@ public class ReadoutController {
         return readoutRepository.save(readout);
     }
 
+    @PatchMapping("/descriptions/{id}")
+    public ResponseEntity<?> updateDescriptionById(@PathVariable("id") Long id,
+                                                   @RequestBody ReadoutDescriptionOnlyDTO description){
+        Optional<Readout> readoutOptional = readoutRepository.findById(id);
+
+        if(readoutOptional.isPresent()){
+            Readout readout = readoutOptional.get();
+            readout.setDescription(description.getDescription());
+
+            return ResponseEntity.ok().body(readoutRepository.save(readout));
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
