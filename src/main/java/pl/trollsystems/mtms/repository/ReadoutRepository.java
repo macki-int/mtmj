56844c6579281fd5752c60b09560bce6fb50f806
marekj <macki@int.pl>
2022-05-29 14:18:43 +0200

    add Sort in ReadoutRepository interface

--- src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
@@ -1,5 +1,6 @@
 package pl.trollsystems.mtms.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.Readout;
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface ReadoutRepository extends JpaRepository<Readout, Long> {
-    List<Readout> findAllByOrderByReadoutDataTime();
+    List<Readout> findAllByOrderByReadoutDataTime(Sort sort);
 }
