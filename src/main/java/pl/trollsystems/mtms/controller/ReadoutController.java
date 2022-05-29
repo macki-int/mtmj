33887615f439bee694c338d13fbd60e7503b59e0
marekj <macki@int.pl>
2022-05-29 14:21:36 +0200

    add parse sort RequestParam in method findAllSortedByReadoutDataTime

--- src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
@@ -1,6 +1,8 @@
 package pl.trollsystems.mtms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.trollsystems.mtms.DTO.ReadoutDescriptionOnlyDTO;
@@ -19,22 +21,27 @@ public class ReadoutController {
     public ReadoutController(ReadoutRepository readoutRepository) {
         this.readoutRepository = readoutRepository;
     }
+
     @GetMapping
-    public ResponseEntity<?> findAllByOrderReadoutDataTime(){
-        return ResponseEntity.ok().body(readoutRepository.findAllByOrderByReadoutDataTime());
+    public ResponseEntity<?> findAllSortedByReadoutDataTime(@RequestParam(value = "sort") String sort) {
+        String[] _sort = sort.split(",");
+        Direction direction = Direction.fromString(_sort[1]);
+        String nameFieldSort = _sort[0];
+
+        return ResponseEntity.ok().body(readoutRepository.findAllByOrderByReadoutDataTime(Sort.by(direction, nameFieldSort)));
     }
 
     @PostMapping
-    public Readout add(@RequestBody Readout readout){
+    public Readout add(@RequestBody Readout readout) {
         return readoutRepository.save(readout);
     }
 
     @PatchMapping("/descriptions/{id}")
     public ResponseEntity<?> updateDescriptionById(@PathVariable("id") Long id,
-                                                   @RequestBody ReadoutDescriptionOnlyDTO description){
+                                                   @RequestBody ReadoutDescriptionOnlyDTO description) {
         Optional<Readout> readoutOptional = readoutRepository.findById(id);
 
-        if(readoutOptional.isPresent()){
+        if (readoutOptional.isPresent()) {
             Readout readout = readoutOptional.get();
             readout.setDescription(description.getDescription());
 
