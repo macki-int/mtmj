00ec592658987f567999e0a4ee9bae7f4d54b864
marekj <macki@int.pl>
2022-06-01 08:21:07 +0200

    add declarations method for statistics in ReadoutRepository interface

--- src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
@@ -2,6 +2,7 @@ package pl.trollsystems.mtms.repository;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.trollsystems.mtms.model.Readout;
 
@@ -10,4 +11,25 @@ import java.util.List;
 @Repository
 public interface ReadoutRepository extends JpaRepository<Readout, Long> {
     List<Readout> findAllByOrderByReadoutDataTime(Sort sort);
+
+    @Query(value = "SELECT max(t_ob1) FROM readouts")
+    Double getMaxTemperature();
+//
+//    @Query(value = "SELECT min(t_ob1) FROM readouts")
+//    Double getMinTemperature();
+//
+//    @Query(value = "SELECT avg(t_ob1) FROM readouts")
+//    Double getAVGTemperature();
+//
+//    @Query(value = "SELECT max(pd) FROM readouts")
+//    Double getMaxWaterLevel();
+//
+//    @Query(value = "SELECT min(pd) FROM readouts")
+//    Double getMinWaterLevel();
+//
+//    @Query(value = "SELECT avg(pd) FROM readouts")
+//    Double getAvgWaterLevel();
+//
+//    @Query(value = "SELECT count(*) FROM readouts")
+//    Integer getCountReadout();
 }
