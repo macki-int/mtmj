9a8e771469671d0f0fbdffbd676ea96db6ba8935
marekj <macki@int.pl>
2022-06-10 23:58:58 +0200

    change queries in ReadoutRepository for max and min values

--- src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
+++ src/main/java/pl/trollsystems/mtms/repository/ReadoutRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.trollsystems.mtms.DTO.MaxTemperatureDTO;
+import pl.trollsystems.mtms.DTO.MaxWaterLevelDTO;
+import pl.trollsystems.mtms.DTO.MinTemperatureDTO;
+import pl.trollsystems.mtms.DTO.MinWaterLevelDTO;
 import pl.trollsystems.mtms.model.Readout;
 
 import java.util.List;
@@ -12,20 +16,24 @@ import java.util.List;
 public interface ReadoutRepository extends JpaRepository<Readout, Long> {
     List<Readout> findAllByOrderByReadoutDataTime(Sort sort);
 
-    @Query(value = "SELECT max(t_ob1), readout_data_time FROM readouts", nativeQuery = true)
-    Double maxTemperature();
+    @Query(value = "SELECT t_ob1, readout_data_time FROM readouts " +
+            "WHERE t_ob1 = (SELECT MAX(t_ob1) FROM readouts)", nativeQuery = true)
+    MaxTemperatureDTO maxTemperature();
 
-    @Query(value = "SELECT min(t_ob1), readout_data_time FROM readouts", nativeQuery = true)
-    Double minTemperature();
+    @Query(value = "SELECT t_ob1, readout_data_time FROM readouts " +
+            "WHERE t_ob1 = (SELECT MIN(t_ob1) FROM readouts)", nativeQuery = true)
+    MinTemperatureDTO minTemperature();
 
     @Query(value = "SELECT avg(t_ob1) FROM readouts", nativeQuery = true)
     Double avgTemperature();
 
-    @Query(value = "SELECT max(pd), readout_data_time FROM readouts", nativeQuery = true)
-    Double maxWaterLevel();
+    @Query(value = "SELECT readout_data_time, pd FROM readouts " +
+            "WHERE pd = (SELECT MAX(pd) FROM readouts)", nativeQuery = true)
+    MaxWaterLevelDTO maxWaterLevel();
 
-    @Query(value = "SELECT min(pd), readout_data_time FROM readouts", nativeQuery = true)
-    Double minWaterLevel();
+    @Query(value = "SELECT readout_data_time, pd FROM readouts " +
+            "WHERE pd = (SELECT MIN(pd) FROM readouts)", nativeQuery = true)
+    MinWaterLevelDTO minWaterLevel();
 
     @Query(value = "SELECT avg(pd) FROM readouts", nativeQuery = true)
     Double avgWaterLevel();
