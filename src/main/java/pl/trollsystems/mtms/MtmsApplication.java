677bdbb0e78c80450e1df3eda8792d694a621de9
marekj <macki@int.pl>
2022-06-11 23:19:46 +0200

    add modelMapper method in Spring configuration

--- src/main/java/pl/trollsystems/mtms/MtmsApplication.java
+++ src/main/java/pl/trollsystems/mtms/MtmsApplication.java
@@ -1,13 +1,20 @@
 package pl.trollsystems.mtms;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MtmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MtmsApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
