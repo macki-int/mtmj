e3b0dd4707dded3bb64668a08343f166678575ce
marekj <macki@int.pl>
2022-06-12 21:08:52 +0200

    remove modelMapper method

--- src/main/java/pl/trollsystems/mtms/MtmsApplication.java
+++ src/main/java/pl/trollsystems/mtms/MtmsApplication.java
@@ -1,9 +1,7 @@
 package pl.trollsystems.mtms;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MtmsApplication {
@@ -11,10 +9,4 @@ public class MtmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MtmsApplication.class, args);
 	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-
 }
