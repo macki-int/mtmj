efbbf70916a9f73a428d6de9d8b8e582243eba7c
marekj <macki@int.pl>
2022-11-08 22:46:16 +0100

    add endpoint /from for filtr by date

--- src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
+++ src/main/java/pl/trollsystems/mtms/controller/ReadoutController.java
@@ -3,12 +3,14 @@ package pl.trollsystems.mtms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.trollsystems.mtms.DTO.*;
 import pl.trollsystems.mtms.model.Readout;
 import pl.trollsystems.mtms.repository.ReadoutRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*")
@@ -29,6 +31,20 @@ public class ReadoutController {
         String nameFieldSort = _sort[0];
 
         return ResponseEntity.ok().body(readoutRepository.findAllByOrderByReadoutDataTime(Sort.by(direction, nameFieldSort)));
+    }
+
+    @GetMapping("/from")
+    public ResponseEntity<?> findAllWithDateFromAndDateToOrderByReadoutDataTime(
+            @RequestParam(value = "dateFrom") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+            @RequestParam(value = "dateTo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo,
+            @RequestParam(value = "sort") String sort) {
+
+        String[] _sort = sort.split(",");
+        Direction direction = Direction.fromString(_sort[1]);
+        String nameFieldSort = _sort[0];
+
+        return ResponseEntity.ok().body(readoutRepository.findAllWithDateFromAndDateToOrderByReadoutDataTime(
+                dateFrom, dateTo, Sort.by(direction, nameFieldSort)));
     }
 
     @GetMapping("/statistics")
